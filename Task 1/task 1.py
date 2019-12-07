b7adf99be4894b1e7bb80b1deec6519d911d06df
siddhartha <siddverma1999@gmail.com>
2019-12-07 12:11:55 +0530

    Task 1 and 2 added

---
+++ Task 1/task 1.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+'''Shakespeare gave the world best of theatrics'''
+
+
+# In[2]:
+
+
+'Shakespeare gave the world best of theatrics'
+
+
+# In[3]:
+
+
+'Shakespeare didn't gave the world best of theatrics'
+
+
+# In[4]:
+
+
+'Shakespeare didn\'t gave the world best of theatrics'
+
+
+# In[5]:
+
+
+''William Shakespeare (bapt. 26 April 1564 – 23 April 1616)[a] was an English poet, playwright, and actor, widely regarded as the greatest writer in the English language and the world's greatest dramatist.[2][3][4] He is often called England's national poet and the "Bard of Avon" (or simply "the Bard").[5][b] His extant works, including collaborations, consist of some 39 plays,[c] 154 sonnets, two long narrative poems, and a few other verses, some of uncertain authorship. His plays have been translated into every major living language and are performed more often than those of any other playwright.[7]''
+
+
+# In[6]:
+
+
+'''William Shakespeare (bapt. 26 April 1564 – 23 April 1616)[a] was an English poet, playwright, and actor, widely regarded as the greatest writer in the English language and the world's greatest dramatist.[2][3][4] He is often called England's national poet and the "Bard of Avon" (or simply "the Bard").[5][b] His extant works, including collaborations, consist of some 39 plays,[c] 154 sonnets, two long narrative poems, and a few other verses, some of uncertain authorship. His plays have been translated into every major living language and are performed more often than those of any other playwright.[7]'''
+
+
+# In[7]:
+
+
+'शाम भी कोई जैसे है नदी लहर लहर जैसे बह रही है'
+
+
+# In[8]:
+
+
+'''शाम भी कोई जैसे है नदी लहर लहर जैसे बह रही है'''
+
+
+# In[9]:
+
+
+''शाम भी कोई जैसे है नदी लहर लहर जैसे बह रही है''
+
+
+# In[10]:
+
+
+'HOMEWORK: readable.io '
+
+
+# In[ ]:
+
+
+
+
