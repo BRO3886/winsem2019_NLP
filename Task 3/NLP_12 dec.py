9f73a1617d8b365fb24fd1abdab3aef32cc5dc9e
siddhartha <siddverma1999@gmail.com>
2019-12-16 10:15:28 +0530

    added task 3

---
+++ Task 3/NLP_12 dec.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import nltk
+
+
+# In[2]:
+
+
+from nltk.corpus import brown
+
+
+# In[3]:
+
+
+brown.categories()
+
+
+# In[6]:
+
+
+s = 0
+for i in brown.categories():
+    s+=1
+s
+
+
+# In[7]:
+
+
+brown.words(categories = 'fiction')
+
+
+# In[8]:
+
+
+len(brown.words(categories = 'adventure'))
+
+
+# In[11]:
+
+
+from nltk.corpus import inaugural
+
+
+# In[12]:
+
+
+inaugural.fileids()
+
+
+# In[13]:
+
+
+inaugural.words(fileids = '1861-Lincoln.txt',3)
+
+
+# In[14]:
+
+
+inaugural.words(fileids = '1861-Lincoln.txt')
+
+
+# In[15]:
+
+
+inaugural.words(fileids = '2009-Obama.txt')
+
+
+# In[16]:
+
+
+inaugural.words(fileids = '2017-Trump.txt')
+
+
+# In[17]:
+
+
+inaugral.words(fileids='2013-Obama.txt')
+
+
+# In[18]:
+
+
+inaugural.words(fileids='2013-Obama.txt')
+
+
+# In[19]:
+
+
+'''
+from nltk.book import *
+f = FreqDist(text1)
+f.mostcommon(10)
+'''
+
+
+# In[1]:
+
+
+from nltk.book import *
+
+
+# In[2]:
+
+
+f = FreqDist(text1)
+
+
+
+# In[5]:
+
+
+f.most_common(10)
+
+
+# In[ ]:
+
+
+
+
